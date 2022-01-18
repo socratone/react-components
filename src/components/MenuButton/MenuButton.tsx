bce6a2a21d4ff43bc7fcd21cf7ea4850d4518b5a
socratone <gim2origin@gmail.com>
2022-01-19 01:52:11 +0900

    fix: 버튼 애니메이션 기능 완성

--- src/components/MenuButton/MenuButton.tsx
+++ src/components/MenuButton/MenuButton.tsx
@@ -10,22 +10,30 @@ const MenuButton = ({ open, onClick }: MenuButtonProps) => {
     <Button
       onClick={onClick}
       style={{
-        animation: open ? '1s rotate-button forwards' : undefined,
+        animation: open
+          ? '1s rotate-button forwards'
+          : '1s reverse-rotate-button forwards',
       }}
     >
       <TopLine
         style={{
-          animation: open ? '.5s rotate-line-2 .8s forwards' : undefined,
+          animation: open
+            ? '.5s rotate-line-2 .7s forwards'
+            : '1s reverse-rotate-line-2 forwards',
         }}
       />
       <BottomLine
         style={{
-          animation: open ? '.5s rotate-line-1 .8s forwards' : undefined,
+          animation: open
+            ? '.5s rotate-line-1 .7s forwards'
+            : '1s reverse-rotate-line-1 forwards',
         }}
       />
       <CenterLine
         style={{
-          animation: open ? '.5s wide-line-3 .8s forwards' : undefined,
+          animation: open
+            ? '.5s narrow-line-3 .7s forwards'
+            : '1s wide-line-3 forwards',
         }}
       />
     </Button>
@@ -36,13 +44,22 @@ const Button = styled.button`
   position: relative;
   border: 0;
   background: transparent;
-  background-color: plum;
-  width: 100px;
-  height: 100px;
+  width: 30px;
+  height: 30px;
   cursor: pointer;
   padding: 0;
 
   @keyframes rotate-button {
+    from {
+      transform: rotate(0);
+    }
+
+    to {
+      transform: rotate(360deg);
+    }
+  }
+
+  @keyframes reverse-rotate-button {
     from {
       transform: rotate(0);
     }
@@ -55,49 +72,79 @@ const Button = styled.button`
 
 const TopLine = styled.div`
   height: 2px;
-  background-color: #0300b6;
+  background-color: #000;
   width: 100%;
   position: absolute;
   top: 50%;
-  transform: translateY(-50%) rotate(-45deg);
+  transform: translateY(calc(-50% - 10px)) rotate(0);
 
   @keyframes rotate-line-2 {
+    from {
+      transform: translateY(calc(-50% - 10px)) rotate(0);
+    }
+
+    to {
+      transform: translateY(-50%) rotate(-45deg);
+    }
+  }
+
+  @keyframes reverse-rotate-line-2 {
     from {
       transform: translateY(-50%) rotate(-45deg);
     }
 
     to {
-      transform: translateY(calc(-50% - 35px)) rotate(0);
+      transform: translateY(calc(-50% - 10px)) rotate(0);
     }
   }
 `;
 
 const BottomLine = styled.div`
   height: 2px;
-  background-color: #e01515;
+  background-color: #000;
   width: 100%;
   position: absolute;
   top: 50%;
-  transform: translateY(-50%) rotate(45deg);
+  transform: translateY(calc(-50% + 10px)) rotate(0);
 
   @keyframes rotate-line-1 {
+    from {
+      transform: translateY(calc(-50% + 10px)) rotate(0);
+    }
+
+    to {
+      transform: translateY(-50%) rotate(45deg);
+    }
+  }
+
+  @keyframes reverse-rotate-line-1 {
     from {
       transform: translateY(-50%) rotate(45deg);
     }
 
     to {
-      transform: translateY(calc(-50% + 35px)) rotate(0);
+      transform: translateY(calc(-50% + 10px)) rotate(0);
     }
   }
 `;
 
 const CenterLine = styled.div`
   height: 2px;
-  background-color: #3c9448;
+  background-color: #000;
   width: 100%;
   position: absolute;
   top: 50%;
-  transform: translateY(-50%) scaleX(0);
+  transform: translateY(-50%) scaleX(1);
+
+  @keyframes narrow-line-3 {
+    from {
+      transform: translateY(-50%) scaleX(1);
+    }
+
+    to {
+      transform: translateY(-50%) scaleX(0);
+    }
+  }
 
   @keyframes wide-line-3 {
     from {
